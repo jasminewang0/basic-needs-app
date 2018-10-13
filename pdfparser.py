330762c7acc2146fae036b77002a3b9841c5efca
Jasmine <jfwang2@ncsu.edu>
2018-10-13 17:07:38 -0400

    need to figure out parsing within each category and variable amount of items

---
+++ pdfparser.py
@@ -0,0 +1,85 @@
+
+# importing required modules 
+import PyPDF2 
+  
+# creating a pdf file object 
+pdfFileObj = open('Wake_County_resources_2017.pdf', 'rb') 
+  
+# creating a pdf reader object 
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
+
+numPages = pdfReader.numPages
+pdfText = ""
+  
+for i in range(1, numPages):
+    # creating a page object 
+    pageObj = pdfReader.getPage(i) 
+
+    # extracting text from page 
+    pdfText += pageObj.extractText()
+
+# Taking out the header
+pdfText = pdfText.split("Childcare/Youth Services\n")[1]
+
+# Childcare/Youth Services
+childcare = pdfText.split("Clothing\n")[0]
+pdfText = pdfText.split("Clothing\n")[1] # rest of text
+
+# Clothing
+clothing = pdfText.split("Education/Training\n")[0]
+pdfText = pdfText.split("Education/Training\n")[1] # rest of text
+
+# Education/Training
+education = pdfText.split("Employment\n")[0]
+pdfText = pdfText.split("Employment\n")[1] # rest of text
+
+# Employment
+employment = pdfText.split("Financial Assistance\n")[0]
+pdfText = pdfText.split("Financial Assistance\n")[1] # rest of text
+
+# Financial Assistance
+financial = pdfText.split("Food\n/Nutrition Assistance\n")[0]
+pdfText = pdfText.split("Food\n/Nutrition Assistance\n")[1] # rest of text
+
+# Food/Nutrition Assistance
+food = pdfText.split("Handicapped/Disabled\n Services\n")[0]
+pdfText = pdfText.split("Handicapped/Disabled\n Services\n")[1] # rest of text
+
+# Handicapped/Disabled Services
+handicapped = pdfText.split("Housing/Home Ownership\n")[0]
+pdfText = pdfText.split("Housing/Home Ownership\n")[1] # rest of text
+
+# Housing/Home Ownership
+housing = pdfText.split("Legal Aid \n")[0]
+pdfText = pdfText.split("Legal Aid \n")[1] # rest of text
+# print(housing.encode('cp1252', errors='replace'))
+
+# Legal Aid
+legal = pdfText.split("Medical Care\n")[0]
+pdfText = pdfText.split("Medical Care\n")[1] # rest of text
+
+# Medical Care
+medical = pdfText.split("Starting a Small Business\n")[0]
+pdfText = pdfText.split("Starting a Small Business\n")[1] # rest of text
+
+# Starting a Small Business
+business = pdfText.split("Transportation\n")[0]
+pdfText = pdfText.split("Transportation\n")[1] # rest of text
+
+# Transportation
+transportation = pdfText.split("Miscellaneous/Other Services \n")[0]
+pdfText = pdfText.split("Miscellaneous/Other Services \n")[1] # rest of text
+
+# Miscellaneous/Other Services
+miscellaneous = pdfText
+# print(miscellaneous)
+
+categories = [childcare, clothing, education, employment, financial,
+              food, handicapped, housing, legal, medical, business,
+              transportation]
+
+for category in categories:
+    print(category)
+
+# closing the pdf file object 
+pdfFileObj.close()
