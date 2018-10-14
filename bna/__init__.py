a52800b7a36e4c04eca555d0701af916d5b95700
Jasmine <jfwang2@ncsu.edu>
2018-10-13 22:04:25 -0400

    added some garbage to the front end. the MAP is BROKEN

--- bna/__init__.py
+++ bna/__init__.py
@@ -27,12 +27,56 @@ def create_app(test_config=None):
     def home():
         return render_template('index.html')
 
-    @app.route('/food')
-    def food_pantries():
-        return render_template('food.html')
+    @app.route('/childcare')
+    def childcare():
+        return render_template('results.html', name="Childcare/Youth Services")
 
-    @app.route('/dmv')
-    def dmv():
-        return redner_template('dmv.html')
+    @app.route('/clothing')
+    def clothing():
+        return render_template('results.html', name="Clothing")
+
+    @app.route('/education')
+    def education():
+        return render_template('results.html', name="Education/Training")
+
+    @app.route('/employment')
+    def employment():
+        return render_template('results.html', name="Employment")
+
+    @app.route('/finaid')
+    def financial_assistance():
+        return render_template('results.html', name="Financial Assistance")
+
+    @app.route('/food')
+    def food():
+        return render_template('results.html', name="Food/Nutrition Assistance")
+
+    @app.route('/handicapped')
+    def handicapped():
+        return redner_template('results.html', name="Handicapped/Disabled Services")
+
+    @app.route('/housing')
+    def housing():
+        return render_template('results.html', name="Housing/Home Ownership")
+
+    @app.route('/legal')
+    def legal():
+        return render_template('results.html', name="Legal Aid")
+
+    @app.route('/medical')
+    def medical():
+        return render_template('results.html', name="Medical Care")
+
+    @app.route('/business')
+    def business():
+        return render_template('results.html', name="Starting a Small Business")
+
+    @app.route('/transportation')
+    def transportation():
+        return render_template('results.html', name="Transportation")
+
+    @app.route('/miscellaneous')
+    def miscellaneous():
+        return render_template('results.html', name="Miscellaneous/Other Services")
 
     return app
